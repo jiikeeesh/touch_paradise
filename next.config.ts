a92150b1bb75cc0a526f0e05bd4a4be2404ad12f
jiikeeesh <78836181+jiikeeesh@users.noreply.github.com>
2026-04-06 23:56:26 +0545

    db changes

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'standalone',
   allowedDevOrigins: ['192.168.1.83'],
+  serverExternalPackages: ["@prisma/client"],
 };
 export default nextConfig;
